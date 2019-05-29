b1cd73993e3f3cbef809893c1d5c6e077d0fc8af
Laurel Fulford <laurelfulford@gmail.com>
2019-05-29 13:16:35 -0700

    Add escaping to strings marked for translation in the header.php file; see #30.

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'newspack' ); ?></a>
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'newspack' ); ?></a>
 
 		<header id="masthead" class="site-header">
 
